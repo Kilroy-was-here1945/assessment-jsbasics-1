7537943c4dca322a846b383ae823b4abd6ffdc14
Dallin Johnson <dallin.jofam@gmail.com>
2022-02-11 12:37:58 -0700

    hello again

--- password.js
+++ password.js
@@ -1,6 +1,33 @@
 console.log("welcome to the password validatior tool")
 console.log("password")
 
+const readline = require('readline');
+
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+
+
+reader.question( function(input){
+    if(input.length > 10) {
+        console.log("wrong")
+    } if(input.length < 10) {
+        console.log("wrong")
+    } if(input.length === 10) {
+        console.log(correct)
+    }
+
+
+    console.log(input.length)
+
+
+
+
+
+}
 
 
 
@@ -18,10 +45,4 @@ console.log("password")
 
 
 
-
-
-
-
-
-
-reader.close()
+);
